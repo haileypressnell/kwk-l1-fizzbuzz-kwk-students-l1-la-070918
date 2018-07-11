813bb275ebb637554061b0551d92e02407c28a4b
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 23:02:28 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,4 +1,8 @@
-amount = 30
-if amount/10 
-  puts Integer amount/10
-  
+# amount = 30
+# if amount/10 
+#   puts Integer amount/10
+
+c_amount = gets.strip
+if c_amount/10
+  puts c_amount/10 + "dimes"
+end
