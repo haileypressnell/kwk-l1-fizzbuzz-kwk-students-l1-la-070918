de750099cb2f1a2ddaa6910f2d23551b2f1210b1
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 23:10:15 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,11 +2,12 @@
 # if amount/10 
 #   puts Integer amount/10
 
-c_amount = gets.strip
+# c_amount = gets.strip
 
-def least_coins(c_amount)
+# def least_coins(c_amount)
+  c_amount = 70
   if c_amount%10 == 0 
     puts c_amount%10 
-  end
-end
-puts least_coins(c_amount)
+#   end
+# end
+# puts least_coins(c_amount)
