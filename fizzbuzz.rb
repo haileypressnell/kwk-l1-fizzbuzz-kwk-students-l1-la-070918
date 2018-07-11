ae6584ed7657b0ba9b2d2677de3a26691e63e8b7
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 22:50:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
-# x=16
-#   if x % 3 == 0 && x % 5 == 0 
-#     puts "FizzBuzz"
-#   elsif x % 5 == 0 
-#     puts "Buzz"
-#   elsif x % 3 == 0 
-#     puts "Fizz"
-#   else
-#     puts "null"
-# end
-
-
-def name(your_name)
-  puts "Hi #{your_name}"
+x=16
+  if x % 3 == 0 && x % 5 == 0 
+    puts "FizzBuzz"
+  elsif x % 5 == 0 
+    puts "Buzz"
+  elsif x % 3 == 0 
+    puts "Fizz"
+  else
+    puts "null"
 end
+
+
