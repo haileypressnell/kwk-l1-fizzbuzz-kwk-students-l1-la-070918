8d03d536b14225a7e55d0aee2772268bdbb95b59
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 23:14:14 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -14,10 +14,10 @@
 
 
 
-least_coins = {
-  :quarters => 0,
-  :dimes => 0,
-  :nickels => 0,
-  :pennies => 0
-}
+# least_coins = {
+#   :quarters => 0,
+#   :dimes => 0,
+#   :nickels => 0,
+#   :pennies => 0
+# }
 
