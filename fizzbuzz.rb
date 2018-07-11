df72eada9655505f71eba3160c3517558d0fd96c
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 22:50:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,15 @@
-x=16
-  if x % 3 == 0 && x % 5 == 0 
-    puts "FizzBuzz"
-  elsif x % 5 == 0 
-    puts "Buzz"
-  elsif x % 3 == 0 
-    puts "Fizz"
-   else
-    puts "null"
-end
+# x=16
+#   if x % 3 == 0 && x % 5 == 0 
+#     puts "FizzBuzz"
+#   elsif x % 5 == 0 
+#     puts "Buzz"
+#   elsif x % 3 == 0 
+#     puts "Fizz"
+#   else
+#     puts "null"
+# end
 
+
+def name(your_name)
+  puts "Hi #{your_name}"
+end
