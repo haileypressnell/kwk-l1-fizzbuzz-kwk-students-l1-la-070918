6c70fc582d3cbb577b4fb084556c7239b09de168
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 23:14:22 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -21,3 +21,41 @@
 #   :pennies => 0
 # }
 
+def least_coins(cents)
+  coins = {}
+  amount = cents
+  
+  if amount >= 25
+    coins[:quarters] = (amount - amount.modulo(25)) / 25
+    amount = amount.modulo(25)
+  else 
+    coins[:quarters] = 0
+  end
+  
+  if amount >= 10 
+    coins[:dimes] = (amount - amount.modulo(10)) / 10
+    amount = amount.modulo(10)
+  else 
+    coins[:dimes] = 0
+  end
+  
+  if amount >= 5 
+    coins[:nickels] = (amount - amount.modulo(5)) / 5
+    amount = amount.modulo(5)
+  else 
+    coins[:nickels] = 0
+  end
+  
+  if amount > 0 
+    coins[:pennies] = amount
+  else 
+    coins[:pennies] = 0
+  end
+  
+  coins #calls the hash coins, so that is what least_coins() is equal to when called
+end
+
+least_coins(29)
+least_coins(99)
+
+
