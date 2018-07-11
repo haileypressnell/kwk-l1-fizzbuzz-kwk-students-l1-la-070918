412d1792b7dab3c3da4a9712a8054138211954f6
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-11 23:17:33 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,17 +1,12 @@
-# amount = 30
-# if amount/10 
-#   puts Integer amount/10
+amount = get.strip
 
-# c_amount = gets.strip
-
-# def least_coins(c_amount)
-  # c_amount = 70
-  # if c_amount%10 == 0 
-  #   puts c_amount%10 
-#   end
-# end
-# puts least_coins(c_amount)
-
+def least_coins(amount)
+  coin_type = {
+    :quarters => 0,
+    :dimes => 0,
+    :nickels => 0,
+    :pennies => 0
+  }
 
 
 
